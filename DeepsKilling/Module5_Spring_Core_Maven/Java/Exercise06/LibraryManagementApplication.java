37c063c2d36726c2dacb23e214e5faf3c2761a17
Arjun-sing8279 <arjunsingh963440@gmail.com>
2026-07-21 18:04:44 +0530

    Module5

---
+++ DeepsKilling/Module5_Spring_Core_Maven/Java/Exercise06/LibraryManagementApplication.java
@@ -0,0 +1,19 @@
+package JavaFSE.DeepsKilling.Module5_Spring_Core_Maven.Java.Exercise06;
+
+import Exercise06.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext6.xml");
+
+        BookService service =
+                context.getBean(BookService.class);
+
+        service.displayService();
+    }
+}
---
+++ DeepsKilling/Module5_Spring_Core_Maven/Java/Exercise06/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package JavaFSE.DeepsKilling.Module5_Spring_Core_Maven.Java.Exercise06.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+
+    public void displayBook() {
+        System.out.println("Book Repository is working");
+    }
+}
