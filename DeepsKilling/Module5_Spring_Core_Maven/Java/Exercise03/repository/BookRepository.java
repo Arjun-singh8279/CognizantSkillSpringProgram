094c95d0e42b49d0844977fb02ac0735c3caac28
Arjun-sing8279 <arjunsingh963440@gmail.com>
2026-07-21 18:03:17 +0530

    Module5

---
+++ DeepsKilling/Module5_Spring_Core_Maven/Java/Exercise03/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package JavaFSE.DeepsKilling.Module5_Spring_Core_Maven.Java.Exercise03.repository;
+
+public class BookRepository {
+
+    public void displayBook() {
+        System.out.println("Book Repository is working");
+    }
+}
---
+++ DeepsKilling/Module5_Spring_Core_Maven/Java/Exercise03/service/BookService.java
@@ -0,0 +1,8 @@
+package JavaFSE.DeepsKilling.Module5_Spring_Core_Maven.Java.Exercise03.service;
+
+public class BookService {
+
+    public void displayService() {
+        System.out.println("Book Service is working");
+    }
+}
